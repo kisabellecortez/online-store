1b06acaa28f25dc15f9c44af291bf481d9e3bd39
kisabellecortez <kisabellecortez@gmail.com>
2025-09-16 01:40:06 -0400

    Changed POST url.

--- store-front-end/src/stripe/checkout-form.js
+++ store-front-end/src/stripe/checkout-form.js
@@ -33,7 +33,7 @@ export default function CheckoutForm({ amount }) {
       return;
     }
 
-    const response = await fetch('http://localhost:4242/create-payment-intent', {
+    const response = await fetch('https://online-store-backend-4gcu.onrender.com/create-payment-intent', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ amount }),
